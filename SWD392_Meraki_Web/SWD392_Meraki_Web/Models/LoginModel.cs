352689ae2f3785a33d0ed5cb485a633d4d90edcf
Bùi Minh Quang <quangbmse171655@fpt.edu.vn>
2024-10-08 13:43:55 +0700

    update login

---
+++ SWD392_Meraki_Web/SWD392_Meraki_Web/Models/LoginModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SWD392_Meraki_Web.Models
+{
+    public class LoginModel
+    {
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public bool Remember { get; set; }
+    }
+}
+
