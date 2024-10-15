a55267745d6456ae9768ea0352220413f5fba575
MamMam03 <155883794+MamMam03@users.noreply.github.com>
2024-10-15 23:17:13 +0700

    Chưa done tại còn rối 1 số cái hôm nào có code hoàn chỉnh những phần list mới hình dung được

---
+++ SWD392_Meraki_Web/SWD392_Meraki_Web/Models/Cart.cs
@@ -0,0 +1,46 @@
+﻿namespace SWD392_Meraki_Web.Models
+{
+    public class CartItem
+    {
+        public string CourtName { get; set; }
+        public Slot slotInCart { get; set; }
+    }
+    public class Cart
+    {
+        List<CartItem> cartItems = new List<CartItem>();
+        List<Court> courts = new List<Court>(); 
+        
+        public IEnumerable<CartItem> CartItems 
+        { 
+            get { return cartItems; } 
+        }
+        public void Add(Slot slot)
+        { 
+            string courtId = slot.CourtId;
+            var item = cartItems.FirstOrDefault(i => i.slotInCart.SlotId == slot.SlotId);
+            var courtName = courts.FirstOrDefault(c => c.CourtId.Equals(courtId)).CourtName;
+
+            if (item == null)
+            {
+                cartItems.Add(new CartItem
+                {
+                    CourtName = courtName,
+                    slotInCart = slot,
+                });
+            }
+        }
+        public void ChangeSlot(Slot slot)
+        {
+
+        }
+        public void Remove(Slot slot)
+        {
+            var item = cartItems.FirstOrDefault(c => c.slotInCart.SlotId == slot.SlotId);
+
+            if (item == null)
+            {
+                cartItems.Remove(item);
+            }
+        }
+    }
+}
