9a2a450a3f3ee88c38ba97895b4d3ac61949a497
Cuong <lemaiquoccuong161101@.com>
2024-10-18 23:34:48 +0700

    Homepage and Some Function

---
+++ SWD392_Meraki_Web/SWD392_Meraki_Web/Controllers/CheckInController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SWD392_Meraki_Web.Models;
+using SWD392_Meraki_Web.Repositories.Interface;
+
+namespace SWD392_Meraki_Web.Controllers
+{
+    public class CheckInController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+
+        public CheckInController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpGet]
+        public IActionResult CheckIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CheckIn(CheckIn checkIn)
+        {
+            if (ModelState.IsValid)
+            {
+                await _userRepository.CheckInUserAsync(checkIn);
+                return RedirectToAction("Success");
+            }
+            return View(checkIn);
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
---
+++ SWD392_Meraki_Web/SWD392_Meraki_Web/Repositories/UserRepository.cs
@@ -0,0 +1,55 @@
+﻿using SWD392_Meraki_Web.DatabaseConnection;
+using SWD392_Meraki_Web.Models;
+using SWD392_Meraki_Web.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace SWD392_Meraki_Web.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly BookingBadmintonContext _context;
+
+        public UserRepository(BookingBadmintonContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User> GetUserByIdAsync(string userId)
+        {
+            return await _context.Users.FindAsync(userId);
+        }
+
+        public async Task AddUserAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateUserAsync(User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteUserAsync(string userId)
+        {
+            var user = await GetUserByIdAsync(userId);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task CheckInUserAsync(CheckIn checkIn)
+        {
+            _context.CheckIns.Add(checkIn);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
